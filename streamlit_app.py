ce8524f874eeaaf7d548365a6f6f2f038ba054e0
Brandon Wellsbury <90425294+wellsbury@users.noreply.github.com>
2024-01-25 17:29:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title='Bs First App',  layout='wide', page_icon=':guitar
 
 t1, t2 = st.columns((0.07,1)) 
 
-t1.image('images/dashboard_icon.png', width = 120)
+t1.image('images/dashboard_icon.png', width = 200)
 t2.title("B's First Application")
 t2.markdown(" **tel:*** ****| website:** https://www.github.com/wellsbury **| email:** mailto:b@github.com")
 
