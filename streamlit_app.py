1d1821f7a440eed5e4c069797cc941f3d8611d38
Brandon Wellsbury <90425294+wellsbury@users.noreply.github.com>
2024-01-25 17:31:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title='Bs First App',  layout='wide', page_icon=':guitar
 #this is the header
  
 
-t1, t2 = st.columns((0.8,1)) 
+t1, t2 = st.columns((0.08,1)) 
 
 t1.image('images/dashboard_icon.png', width = 200)
 t2.title("B's First Application")
