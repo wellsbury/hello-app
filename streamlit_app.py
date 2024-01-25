1bda8107eec53ee491d7e35b8c966d9f7df3973c
Brandon Wellsbury <90425294+wellsbury@users.noreply.github.com>
2024-01-25 17:08:38 -0500

    Update streamlit_app.py
    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,11 @@ st.header('Testing Data Vizualiation')
 st.subheader('My Charts')
 
 # Replace 'your_file.csv' with the actual path to your CSV file
-file_path = 'https://github.com/wellsbury/hello-app/blob/c04ed3787cb78ea22677397419a607e882081236/streamlit_test_data.csv'
+file_path = 'https://raw.githubusercontent.com/wellsbury/hello-app/main/streamlit_test_data.csv'
 
 # Read the CSV file into a DataFrame
 df = pd.read_csv(file_path)
 # Display the DataFrame
-print(df.head())
+st.dataframe(df)
+
 
