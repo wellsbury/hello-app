5eff53c04c039211b0479cb9ff4af4851148e504
Brandon Wellsbury <90425294+wellsbury@users.noreply.github.com>
2024-02-01 11:23:12 -0500

    Update streamlit_app.py
    colapse menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,15 +13,18 @@ t2.title("B's First Application")
 t2.markdown(" **tel:*** ****| website:** https://www.github.com/wellsbury **| email:** mailto:b@github.com")
 
 
+## Data
+
 st.header('Testing Data Vizualiation')
 st.subheader('My Charts')
 
-# Replace 'your_file.csv' with the actual path to your CSV file
-file_path = 'https://raw.githubusercontent.com/wellsbury/hello-app/main/streamlit_test_data.csv'
-
-# Read the CSV file into a DataFrame
-df = pd.read_csv(file_path)
-# Display the DataFrame
-st.dataframe(df)
+with st.expander("Hidden Data Table"):
+ # Replace 'your_file.csv' with the actual path to your CSV file
+ file_path = 'https://raw.githubusercontent.com/wellsbury/hello-app/main/streamlit_test_data.csv'
+ 
+ # Read the CSV file into a DataFrame
+ df = pd.read_csv(file_path)
+ # Display the DataFrame
+ st.dataframe(df)
 
 
