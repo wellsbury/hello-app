6b9239580c9621a7b0cf2882f5cc82a3ffdeba49
Brandon Wellsbury <90425294+wellsbury@users.noreply.github.com>
2024-02-01 22:08:31 +0000

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
 
 st.set_page_config(page_title='Bs First App', layout='wide', page_icon=':guitar:')
 
@@ -18,14 +20,45 @@ with t2:
 st.header('Testing Data Visualization')
 st.subheader('My Charts')
 
-with st.expander("Hidden Data Table", expanded=True):
-    # Replace 'your_file.csv' with the actual path to your CSV file
-    file_path = 'https://raw.githubusercontent.com/wellsbury/hello-app/main/streamlit_test_data.csv'
-
-    # Read the CSV file into a DataFrame
-    df = pd.read_csv(file_path)
+with st.spinner('Updating Report...'):
     
-    # Display the DataFrame
-    st.dataframe(df)
+    #Metrics setting and rendering
+
+    
+    m1, m2 = st.columns((1,2))
+    
+    excel_file_path = "membership_data.xlsx"  # Replace with the actual path
+    df = pd.read_excel(excel_file_path, sheet_name="Sheet_0")
+
+ 
+    # Extract the value from cell A1
+    value_a1 = df.iloc[0, 0]
+
+
+
+    # Display the value in Streamlit
+    m1.metric("Total Users", value_a1)
+
+    fgdf = pd.read_excel('membership_data.xlsx', sheet_name='Sheet_1')
+
+    # Sort the DataFrame by the 'Count' column in descending order
+    fgdf = fgdf.sort_values(by='Count', ascending=True)
+
+    # Take the top 20 entries
+    top_20_df = fgdf.tail(20)
+
+    fig = px.bar(top_20_df, x='Count', y='Job Title', template='seaborn',
+                labels={'count': 'Job Title'}, orientation='h',
+                category_orders={'Job Title': list(reversed(top_20_df['Job Title'].tolist()))})
+
+    fig.update_traces(marker_color='#264653')
+
+    fig.update_layout(title_text="Top 20 Customers by Job Title", title_x=0, margin=dict(l=0, r=10, b=10, t=30),
+                    yaxis_title=None, xaxis_title=None)
+
+    m2.plotly_chart(fig, use_container_width=True)
+
+
+
 
 
